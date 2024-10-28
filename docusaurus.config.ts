c2b57bc93460d814d5c3a91664cfc415ef2b9267
Bastien Lopez <bastien.lopez40@gmail.com>
2024-10-28 14:15:06 +0100

    up welcome + versioning

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,7 +60,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'documentationSidebar',
           position: 'left',
-          label: 'Documentation v0.0.1',
+          label: 'Documentation v0.1.0',
         },
         {
           href: 'https://github.com/TalaoDAO',
